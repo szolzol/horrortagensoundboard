8fa2d1c385fdc72d62f3e01c90c792825cbf0477
szolzol <zoltan.szoleczki@cemsmail.org>
2025-08-23 22:02:54 +0200

    Manual edits and asset updates (sound files, CSS, font)

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,7 +1,5 @@
-// Dynamikus baseUrl meghatározása, hogy localban és GitHub Pages-en is működjön
+// Egyszerűsített elérési út: csak relatív path, hogy localban és GitHub Pages-en is működjön
 (function () {
-  // baseUrl: pl. "/horrortagensoundboard/" vagy "/"
-  var baseUrl = window.location.pathname.replace(/\/[^\/]*$/, '/')
   var numberOfHorrorButtons = document.querySelectorAll(".horroricon").length;
   for (var i = 0; i < numberOfHorrorButtons; i++) {
     document
@@ -20,180 +18,160 @@
     switch (key) {
       case "q":
         var sound1 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[0].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[0].innerHTML +
             ".mp3"
         );
         sound1.play();
         break;
       case "w":
         var sound2 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[1].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[1].innerHTML +
             ".mp3"
         );
         sound2.play();
         break;
       case "e":
         var sound3 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[2].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[2].innerHTML +
             ".mp3"
         );
         sound3.play();
         break;
       case "r":
         var sound4 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[3].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[3].innerHTML +
             ".mp3"
         );
         sound4.play();
         break;
       case "t":
         var sound5 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[4].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[4].innerHTML +
             ".mp3"
         );
         sound5.play();
         break;
       case "z":
         var sound6 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[5].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[5].innerHTML +
             ".mp3"
         );
         sound6.play();
         break;
       case "u":
         var sound7 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[6].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[6].innerHTML +
             ".mp3"
         );
         sound7.play();
         break;
       case "i":
         var sound8 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[7].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[7].innerHTML +
             ".mp3"
         );
         sound8.play();
         break;
       case "o":
         var sound9 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[8].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[8].innerHTML +
             ".mp3"
         );
         sound9.play();
         break;
       case "p":
         var sound10 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[9].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[9].innerHTML +
             ".mp3"
         );
         sound10.play();
         break;
       case "a":
         var sound11 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[10].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[10].innerHTML +
             ".mp3"
         );
         sound11.play();
         break;
       case "s":
         var sound12 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[11].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[11].innerHTML +
             ".mp3"
         );
         sound12.play();
         break;
       case "d":
         var sound13 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[12].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[12].innerHTML +
             ".mp3"
         );
         sound13.play();
         break;
       case "f":
         var sound14 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[13].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[13].innerHTML +
             ".mp3"
         );
         sound14.play();
         break;
       case "g":
         var sound15 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[14].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[14].innerHTML +
             ".mp3"
         );
         sound15.play();
         break;
       case "h":
         var sound16 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[15].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[15].innerHTML +
             ".mp3"
         );
         sound16.play();
         break;
       case "j":
         var sound17 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[16].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[16].innerHTML +
             ".mp3"
         );
         sound17.play();
         break;
       case "k":
         var sound18 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[17].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[17].innerHTML +
             ".mp3"
         );
         sound18.play();
         break;
       case "l":
         var sound19 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[18].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[18].innerHTML +
             ".mp3"
         );
         sound19.play();
         break;
       case "é":
         var sound20 = new Audio(
-          baseUrl +
-            "sounds/" +
-            encodeURIComponent(document.querySelectorAll(".horroricon")[19].innerHTML) +
+          "sounds/" +
+            document.querySelectorAll(".horroricon")[19].innerHTML +
             ".mp3"
         );
         sound20.play();
